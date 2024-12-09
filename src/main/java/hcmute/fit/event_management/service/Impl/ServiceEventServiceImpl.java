2b9ef8c0ea366cb2903fba1226b012dd08c33581
Minh Trung <trungho.234416@gmail.com>
2024-12-10 01:39:09 +0700

    update provider-service

--- src/main/java/hcmute/fit/event_management/service/Impl/ServiceEventServiceImpl.java
+++ src/main/java/hcmute/fit/event_management/service/Impl/ServiceEventServiceImpl.java
@@ -9,10 +9,13 @@ import hcmute.fit.event_management.repository.EventRepository;
 import hcmute.fit.event_management.repository.ProviderServiceRepository;
 import hcmute.fit.event_management.repository.ServiceEventRepository;
 import hcmute.fit.event_management.service.IServiceEventSerivce;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,15 +46,20 @@ public class ServiceEventServiceImpl implements IServiceEventSerivce {
         return dtoList;
     }
     @Override
-    public boolean addServiceEvent(int eventId, int serviceId) {
+    public boolean addServiceEvent(ServiceEventDTO serviceEventDTO) {
         boolean result = false;
         try {
-            Optional<ProviderService> service = providerServiceRepository.findById(serviceId);
-            Optional<Event> event = eventRepository.findById(eventId);
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Optional<ProviderService> service = providerServiceRepository.findById(serviceEventDTO.getServiceId());
+            Optional<Event> event = eventRepository.findById(serviceEventDTO.getEventId());
             ServiceEvent serviceEvent = new ServiceEvent();
-            ProviderServiceEventId providerServiceEventId = new ProviderServiceEventId( serviceId,eventId);
+            ProviderServiceEventId providerServiceEventId = new ProviderServiceEventId( serviceEventDTO.getServiceId(),serviceEventDTO.getEventId());
             serviceEvent.setId(providerServiceEventId);
             serviceEvent.setService(service.get());
+            Date rentalDate = formatter.parse(serviceEventDTO.getRentalDate());
+            serviceEvent.setRentalDate(rentalDate);
+            Date expDate = formatter.parse(serviceEventDTO.getExpDate());
+            serviceEvent.setExpDate(expDate);
             serviceEvent.setEvent(event.get());
 
             serviceEventRepository.save(serviceEvent);
@@ -75,6 +83,16 @@ public class ServiceEventServiceImpl implements IServiceEventSerivce {
             System.out.println("Delete failed"+e.getMessage());
         }
         return result;
+    }
+    @Override
+    public ServiceEventDTO detailRental(int eventId, int serviceId) {
+        ServiceEvent tmp =  serviceEventRepository.serviceEvent(eventId,serviceId);
+        ServiceEventDTO dto = new ServiceEventDTO();
+        dto.setEventId(tmp.getEvent().getEventID());
+        dto.setServiceId(tmp.getService().getId());
+        dto.setRentalDate(tmp.getRentalDate().toString());
+        dto.setExpDate(tmp.getExpDate().toString());
+        return dto;
     }
 
 }
